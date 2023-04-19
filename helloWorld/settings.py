4d084a679a75bb6d139fa694e151fb1509730d78
PAUL <you@example.com>
2023-04-19 15:20:00 +0300

    whitenoise

--- helloWorld/settings.py
+++ helloWorld/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    #whitenoise
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
@@ -53,6 +57,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #whitenoise
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'helloWorld.urls'
