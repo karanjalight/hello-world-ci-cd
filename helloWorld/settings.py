60dba7323354abfc9a073f2da1d797d177a167c6
PAUL <you@example.com>
2023-04-19 15:11:35 +0300

    settingss

--- helloWorld/settings.py
+++ helloWorld/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
+import os
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,9 +82,11 @@ WSGI_APPLICATION = 'helloWorld.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
